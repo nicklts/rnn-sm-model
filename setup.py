20277cb759dcbb93debe6a32231b3fdfb9c191b6
nicklts <84441267+nicklts@users.noreply.github.com>
2021-08-19 15:14:47 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+  name="soil-moisture-rnn",
+  version="1.0.0",
+  author="Nicklas Simonsen",
+  author_email="nicklas0693@gmail.com",
+  description="RNN-based deep learning model for soil moisture prediction",
+  url="https://github.com/nicklts/RNN-SM-Model",
+  packages=find_packages(),
+)
+  
